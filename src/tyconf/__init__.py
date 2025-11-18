e357ed0d27d70576fab810e1d84514ad36116372
Andrzej Barabasz <abarabasz@gmail.com>
2025-11-18 11:00:06 +0100

    Bump version to 1.0.2

--- src/tyconf/__init__.py
+++ src/tyconf/__init__.py
@@ -8,7 +8,7 @@ configuration with runtime type validation and an intuitive API.
 from .core import TyConf, PropertyDescriptor
 
 __all__ = ["TyConf", "PropertyDescriptor"]
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __date__ = "2025-11-18"
 __author__ = "barabasz"
 __license__ = "MIT"
